02ca02e878a44cd2a3687956ca52532b7c0cd86d
Troy Goode <troygoode@gmail.com>
2012-06-22 19:22:55 -0400

    added built-in support for regex validation

---
+++ test/validate_regex.js
@@ -0,0 +1,95 @@
+var should = require('should')
+  , tsa = require('../');
+
+describe('Validations: Regex', function(){
+  var guard = tsa({
+    foo: tsa.required({ validate: tsa.validate.regex(/^bar$/) })
+  });
+
+  it('positive case', function(done){
+    //arrange
+    var input = { foo: 'bar' };
+
+    //act
+    guard().frisk(input, function(err, result){
+      //assert
+      should.not.exist(err);
+      should.exist(result);
+      result.foo.should.equal('bar');
+
+      done();
+    });
+  });
+
+  it('negative case', function(done){
+    //arrange
+    var input = { foo: 'baz' };
+
+    //act
+    guard().frisk(input, function(err, result){
+      //assert
+      should.exist(err);
+      err[0].key.should.equal('foo');
+      err[0].error.should.equal('Pattern did not match: /^bar$/.');
+      should.not.exist(result);
+
+      done();
+    });
+  });
+
+  it('custom error message', function(done){
+    //arrange
+    var guard = tsa({
+      foo: tsa.required({ validate: tsa.validate.regex(/^bar$/, 'well... crap') })
+    });
+    var input = { foo: 'baz' };
+
+    //act
+    guard().frisk(input, function(err, result){
+      //assert
+      should.exist(err);
+      err[0].key.should.equal('foo');
+      err[0].error.should.equal('well... crap');
+      should.not.exist(result);
+
+      done();
+    });
+  });
+
+  it('positive case - string instead of regex', function(done){
+    //arrange
+    var guard = tsa({
+      foo: tsa.required({ validate: tsa.validate.regex("^bar$") })
+    });
+    var input = { foo: 'bar' };
+
+    //act
+    guard().frisk(input, function(err, result){
+      //assert
+      should.not.exist(err);
+      should.exist(result);
+      result.foo.should.equal('bar');
+
+      done();
+    });
+  });
+
+  it('negative case - string instead of regex', function(done){
+    //arrange
+    var guard = tsa({
+      foo: tsa.required({ validate: tsa.validate.regex("^bar$") })
+    });
+    var input = { foo: 'baz' };
+
+    //act
+    guard().frisk(input, function(err, result){
+      //assert
+      should.exist(err);
+      err[0].key.should.equal('foo');
+      err[0].error.should.equal('Pattern did not match: /^bar$/.');
+      should.not.exist(result);
+
+      done();
+    });
+  });
+});
